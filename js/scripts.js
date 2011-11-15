aae12d5f2ec1ba301fc08f24688a37c276d8e757
James McKinney <james@slashpoundbang.com>
2011-11-15 13:17:09 -0500

    switch to new design

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,4 @@
 $(function () {
-  var scroller;
-  $('html,body').each(function () {
-    var element = $(this);
-    if (element.scrollTop() > 0) {
-      scroller = element;
-    }
-    else {
-      element.scrollTop(1);
-      var test = element.scrollTop() > 0;
-      element.scrollTop(0);
-      if (test) {
-        scroller = element;
-      }
-    }
-  });
-  $('a.back').click(function (event) {
-    scroller.animate({scrollTop: 0}, 700);
-    event.preventDefault();
-  });
-    
   $('#slider').fadeIn(300).nivoSlider({
     effect: 'fold',
     pauseTime: 5000,
